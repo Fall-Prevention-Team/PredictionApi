a2c342f285650185b29581d2b3d6244751f384aa
HowToYEET <kkapke19@student.aau.dk>
2022-03-29 12:46:11 +0200

    make it connect to discord, go play buenno

--- message_BOT.py
+++ message_BOT.py
@@ -28,23 +28,71 @@ async def on_message(message):
         return
     
     root = os.path.join(os.path.dirname(__file__), os.path.join('logs', "tsvs"))
-    if str(message.author) == "Kenneth Køpke#8877":
-        print("det er bueno der skriver...")
-        #await message.channel.send("```Hej Bueno... im a bot```")
-
-    print(message.author)
     logs = os.listdir(root)
-    print(logs)
-    print(str(message.content) + ".tsv")
-    if str(message.content) + ".tsv" in logs:
-        print("HERE")
-
-    #dataframe = pd.read_csv(root + str(message) + ".tsv")
-    #print("``` " + dataframe + "```")
+    content = message.content
 
 
+    if str(content) == "test":
+        print("det er bueno der skriver...")
+        await message.channel.send("```test```")
+    
+    if content.startswith('!'):
+        print("HERE")           
+        for l in logs:
+            name_of_file = l.split('.')[0]
+            if name_of_file in content:
+                print("H")
+                await message.channel.send("FILE FOUND MATCHING YOUR STRING:  " +str(l))
+
+    if content in logs:
+        df = pd.read_csv(root + "\\"+ content, sep="\t", header=None)
+        print(df.to_string())
+        await message.channel.send(df.to_string())
+    
+    if content == "forrest...":
+        await message.channel.send(Run_Forrest_Run())
+        
+        
+
+
+
+    if "HELP" in content.upper():
+        await message.channel.send("``` INFO: \n !some_string = find a matching file on user id \n some_file_name = give data from file ```")
+
+        
+
+            
 
     
-    
-    
+
+    #if str(message.content) + ".tsv" in logs:
+
+
+#@bot_connection.event
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import SelectFromModel
+import pickle, sys  
+def Run_Forrest_Run():
+    root = os.path.join(os.path.dirname(__file__), os.path.join('logs', "tsvs"))
+    test_data = pd.read_csv(root + '/out_TEST.tsv', sep='\t', header=None)
+    train_data = pd.read_csv(root + '/out_TRAIN.tsv', sep='\t', header=None)
+    y_test = test_data[0]
+    x_test = test_data.loc[:, test_data.columns != 0]
+    y_train = train_data[0]
+    x_train = train_data.loc[:, test_data.columns != 0]
+    feat_clf = SelectFromModel(RandomForestClassifier(n_estimators=20))
+    clf = RandomForestClassifier(n_estimators=20)
+    clf.fit(x_train, y_train)
+    feat_clf.fit(x_train, y_train)
+    features_selected = x_train.columns[(feat_clf.get_support())]
+    print(len(features_selected))
+    print(features_selected)
+    print(clf.estimators_[0])
+    prediction_of_y = clf.predict(x_test)
+    acc = accuracy_score(y_true=y_test, y_pred=prediction_of_y)
+    return acc
+
+
 bot_connection.run(Token)
