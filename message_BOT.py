69a47b6488d7a8747252efe3dffa87fb554baf46
HowToYEET <kkapke19@student.aau.dk>
2022-04-13 14:41:29 +0200

    yes made class
    yes

--- message_BOT.py
+++ message_BOT.py
@@ -4,7 +4,7 @@ import discord
 import pandas as pd
 from dotenv import load_dotenv
 from pathlib import Path
-
+from tsvmerge_forester import Tsv_handler
 
 load_dotenv()
 Token = os.getenv('DISCORD_TOKEN')
@@ -73,8 +73,8 @@ async def on_message(message):
             await message.channel.send("no such is on a watch: " + str(watch_id))
         
 
-    if content == "forest...":
-        await message.channel.send(Run_Forrest_Run())
+    if content == "give me stats":
+        await message.channel.send(Tsv_handler.Run_Forrest_Run())
         
 
 
@@ -82,33 +82,6 @@ async def on_message(message):
 
     if "HELP" in content.upper():
         await message.channel.send("``` INFO: \n !some_string = find a matching file on user id \n some_tsvfile_name = give data from file \n  update personal_logs.json names = !name wacth_id name```")
-
-
-#@bot_connection.event
-from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
-from sklearn.metrics import accuracy_score, consensus_score
-from sklearn.feature_selection import SelectFromModel
-import pickle, sys  
-def Run_Forrest_Run():
-    root = os.path.join(os.path.dirname(__file__), os.path.join('logs', "tsvs"))
-    test_data = pd.read_csv(root + '/out_TEST.tsv', sep='\t', header=None)
-    train_data = pd.read_csv(root + '/out_TRAIN.tsv', sep='\t', header=None)
-    y_test = test_data[0]
-    x_test = test_data.loc[:, test_data.columns != 0]
-    y_train = train_data[0]
-    x_train = train_data.loc[:, test_data.columns != 0]
-    feat_clf = SelectFromModel(RandomForestClassifier(n_estimators=20))
-    clf = RandomForestClassifier(n_estimators=20)
-    clf.fit(x_train, y_train)
-    feat_clf.fit(x_train, y_train)
-    features_selected = x_train.columns[(feat_clf.get_support())]
-    print(len(features_selected))
-    print(features_selected)
-    print(clf.estimators_[0])
-    prediction_of_y = clf.predict(x_test)
-    acc = accuracy_score(y_true=y_test, y_pred=prediction_of_y)
-    return acc
 
 
 bot_connection.run(Token)
--- tsvmerge_forester.py
+++ tsvmerge_forester.py
@@ -1,55 +1,59 @@
 import pandas as pd
 import os
-
-root = os.path.join(os.path.dirname(__file__), "logs", "tsvs")
-files = [os.path.join(root, f) for f in os.listdir(root) if f.endswith(".tsv")]
-
-
-def read_all_tsvs():
-    list_of_df =[pd.read_csv(o, sep="\t", header=None) for o in files]
-    big_data_df = pd.concat(list_of_df, axis=0, ignore_index=True)
-    print(big_data_df.shape)
-    print(big_data_df)
-    df = big_data_df.iloc[: , 1:]
-    return df
-
-
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split
-def Run_Forrest_Run():
-    data = read_all_tsvs()
-    X = data.loc[:, data.columns != 1]
-    y = data.iloc[:, 0]
-    print(X)
-    print(y)
-    x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.1)
-    feat_clf = SelectFromModel(RandomForestClassifier(n_estimators=20))
-    clf = RandomForestClassifier(n_estimators=20)
-    clf.fit(x_train, y_train)
-    feat_clf.fit(x_train, y_train)
-    features_selected = x_train.columns[(feat_clf.get_support())]
-    print(len(features_selected))
-    print(features_selected)
-    print(clf.estimators_[0])
-    prediction_of_y = clf.predict(x_test)
-    acc = accuracy_score(y_true=y_test, y_pred=prediction_of_y)
-    print(acc)
-    return acc
+
+root = os.path.join(os.path.dirname(__file__), "logs", "tsvs")
+files = [os.path.join(root, f) for f in os.listdir(root) if f.endswith(".tsv")]
+
+path_to_drop_tsv = os.path.join(os.path.dirname(__file__),"logs", "split")
+print(path_to_drop_tsv)
+class Tsv_handler:
+
+    def read_all_tsvs():
+        list_of_df =[pd.read_csv(o, sep="\t", header=None) for o in files]
+        big_data_df = pd.concat(list_of_df, axis=0, ignore_index=True)
+        print(big_data_df.shape)
+        print(big_data_df)
+        df = big_data_df.iloc[: , 1:]
+        return df
+
+
+
+    def Run_Forrest_Run():
+        data = Tsv_handler.read_all_tsvs()
+        X = data.loc[:, data.columns != 1]
+        y = data.iloc[:, 0]
+        print(X)
+        print(y)
+        x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.25)
+        feat_clf = SelectFromModel(RandomForestClassifier(n_estimators=100))
+        clf = RandomForestClassifier(n_estimators=100)
+        clf.fit(x_train, y_train)
+        feat_clf.fit(x_train, y_train)
+        features_selected = x_train.columns[(feat_clf.get_support())]
+        print(len(features_selected))
+        print(features_selected)
+        print(clf.estimators_[0])
+        prediction_of_y = clf.predict(x_test)
+        acc = accuracy_score(y_true=y_test, y_pred=prediction_of_y)
+        print(acc)
+        return acc
 
 
 
 
-#Run_Forrest_Run()
 
-if __name__ == "__main__":
-    data_to_split = read_all_tsvs()
-    train_data = data_to_split.sample(frac=0.5)
-    test_data = data_to_split.drop(train_data.index)
-    train_data.to_csv("./aout_TRAIN.tsv",sep="\t", header=None, index=False)
-    test_data.to_csv("./aout_TEST.tsv",sep="\t", header=None, index=False)
+    if __name__ == "__main__":
+        fname = "aout"
+        data_to_split = read_all_tsvs()
+        train_data = data_to_split.sample(frac=0.5)
+        test_data = data_to_split.drop(train_data.index)
+        train_data.to_csv(os.path.join(path_to_drop_tsv, f"{fname}_TRAIN.tsv"),sep="\t", header=None, index=False)
+        test_data.to_csv(os.path.join(path_to_drop_tsv, f"{fname}TEST.tsv"),sep="\t", header=None, index=False)
     
 
     
