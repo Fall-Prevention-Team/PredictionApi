08460724504fa4d9f0b03f72e233b57c6f5a6adb
HowToYEET <kkapke19@student.aau.dk>
2022-05-03 12:01:13 +0200

    Update MakeGraph.py

--- MakeGraph.py
+++ MakeGraph.py
@@ -3,6 +3,7 @@ from cProfile import label
 import os
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler as scale
+from sklearn.preprocessing import RobustScaler as Robust
 import pandas as pd
 
 root = os.path.join(os.path.dirname(__file__), "logs", "tsvs")
@@ -19,7 +20,9 @@ def read_all_tsvs():
 def GraphFromTSV():
         data_frame = read_all_tsvs()            
         sc = scale()
+        Ro = Robust()
         scale_dataframe = sc.fit_transform(data_frame)
+        Robust_dataframe = Ro.fit_transform(data_frame)
         #plt.subplot(3,1,1)
         x_values = data_frame.iloc[:, 1::3]
         y_values = data_frame.iloc[:, 2::3]
@@ -29,11 +32,14 @@ def GraphFromTSV():
             x_scale = scale_dataframe[index][1::3]
             y_scale = scale_dataframe[index][2::3]
             z_scale = scale_dataframe[index][3::3]
+            x_robust = Robust_dataframe[index][1::3]
+            y_robust = Robust_dataframe[index][2::3]
+            z_robust = Robust_dataframe[index][3::3]
             x_raw = x_values.loc[index]
             y_raw = y_values.loc[index]
             z_raw = z_values.loc[index]
 
-            fig, (raw_plot, scale_plot) = plt.subplots(2)
+            fig, (raw_plot, scale_plot, Robust_plot) = plt.subplots(3)
 
             raw_plot.plot(x_raw, label="X label")
             raw_plot.plot(y_raw, label="Y label")
@@ -45,9 +51,17 @@ def GraphFromTSV():
             scale_plot.plot(z_scale, label="Z label")
             scale_plot.set_title("scaled data")
 
+            Robust_plot.plot(x_robust, label="X label")
+            Robust_plot.plot(y_robust, label="Y label")
+            Robust_plot.plot(z_robust, label="Z label")
+            Robust_plot.set_title("Robust data")
+
             fig.suptitle("Fall number: " +str(index+1))
             raw_plot.legend()
             scale_plot.legend()
+            Robust_plot.legend()
+            print(x_robust)
+            print(x_scale)
             plt.show()
 
         
